22b652946111208a776d35d53102bc7b4d3a47e6
MyCode07 <erem19992015@mail.ru>
2024-05-09 09:21:27 +0300

    first log

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+// import { maskInputs } from "./static/inputmask.js";
+// import { replaceDomElements } from "./static/replace.js";
+// import { animateAction, animateStaggerAction } from "./parts/animations.js";
+
+// import "./parts/popup.js";
+// import "./parts/menu.js";
+// import "./parts/more-photos.js";
+
+// animateAction();
+// animateStaggerAction();
+// replaceDomElements();
+
+maskInputs('+7 (999) 999-99-99', '._mask-phone')
+
+import { Fancybox } from "@fancyapps/ui";
+Fancybox.bind("[data-fancybox]", {
+});
+
+const inputItems = [...document.querySelectorAll('input ')].concat([...document.querySelectorAll('textarea ')])
+if (inputItems.length) {
+    inputItems.forEach(input => {
+        if (input.closest('.form__item')) {
+            input.addEventListener('input', () => {
+                console.log(input.value);
+                if (input.value != '') {
+                    input.classList.add('_active')
+                }
+                else {
+                    input.classList.remove('_active')
+                }
+            })
+        }
+    })
+}
+
+
+document.addEventListener('click', function (e) {
+    let targetEl = e.target;
+    if (targetEl.classList.contains('pages-close')) {
+        document.querySelector('.pages').classList.toggle('_hide');
+    }
+})
